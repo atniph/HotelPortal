b729867fc91c7c18cafdbef17d3aef310e852a9a
messgeri <messgeri@gmail.com>
2019-12-01 00:46:18 +0100

    JavaScript data & price validation

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,42 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function ValidateForm(from, to) {
+    var first = new Date(document.forms["form"][from].value);
+    var last = new Date(document.forms["form"][to].value);
+    var today = new Date();
+    today.setHours(0, 0, 0, 0);
 
-// Write your JavaScript code.
+    if (first >= last) {
+        alert("First day must be before last day!");
+        return false;
+    }
+    if (first < today) {
+        alert("You can't reserve a room from the past");
+        return false;
+    }
+}
+
+function ValidateSearch() {
+    var min = Number(document.forms["form"]["min"].value);
+    var max = Number(document.forms["form"]["max"].value);
+
+    if (!max) {
+        return ValidateForm("from", "to");
+    }
+
+    if (min > max) {
+        alert("Maximum price should be higher than minimum!");
+        return false;
+    }
+    return ValidateForm("from", "to");
+}
+
+function ValidateReserve() {
+    var succes = ValidateForm("FirstDay", "LastDay");
+    if (succes == false) {
+
+        return false;
+    } else {
+
+        alert("Succesful reserve");
+        return true;
+    } 
+}
